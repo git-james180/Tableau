4df2d1fa46ba6130a26c4f359331e1580727ed37
git-james180 <75849462+git-james180@users.noreply.github.com>
2024-06-07 19:26:05 +0100

    Add files via upload

---
+++ standard.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Declaring _arg1 as the df needs to be the first step.
+#df = pd.DataFrame(_arg1)
+
+#make any changes here.
+
+
+#this is the standard way to exit.
+#return df.to_dict(orient='list')
